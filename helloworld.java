feb57027fde2ca96c56b7f973ef726f413f184b7
BHAGYAJAYAPAL <49110396+BHAGYAJAYAPAL@users.noreply.github.com>
2022-03-15 16:02:14 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
-    public stati void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Have a nice day");
     }
